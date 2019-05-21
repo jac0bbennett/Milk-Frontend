7f7eb79460a1f9bda06b3ad877eb73222ac24e80
Jacob Bennett <jacobwbennett@gmail.com>
2019-05-21 03:59:13 -0600

    Show draft or published

--- src/components/Views/Content/contentItem.js
+++ src/components/Views/Content/contentItem.js
@@ -15,12 +15,12 @@ const ContentItem = props => {
         <h3 className="softtext" title={new Date(props.content.editedAt)}>
           <TimeAgoStamp>{props.content.editedAt}</TimeAgoStamp>
         </h3>
-        {props.content.status === 0 ? (
+        {props.content.status === 0 ||
+        (props.content.editedAt !== props.content.publishedAt ||
+          props.content.editedAt !== props.content.updatedAt) ? (
           <span className="softtext">Draft</span>
         ) : (
-          <span className="softtext" style={{ color: "#3cc883" }}>
-            Published
-          </span>
+          <span className="greentext">Published</span>
         )}
       </div>
     </Link>
