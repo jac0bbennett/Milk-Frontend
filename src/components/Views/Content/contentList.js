7bdc5833a25ef3ff92c43c8a2e0efdec3b4bc303
Jacob Bennett <jacobwbennett@gmail.com>
2019-06-24 22:56:07 -0600

    Content type sort added

--- src/components/Views/Content/contentList.js
+++ src/components/Views/Content/contentList.js
@@ -217,6 +217,7 @@ const ContentList = props => {
                 >
                   <option value="dateDescending">Last edited</option>
                   <option value="dateAscending">Oldest edited</option>
+                  <option value="contenttype">Content Type</option>
                 </DropDownInput>
               </span>
               <span className="contentstatus" style={{ marginLeft: "20px" }}>
