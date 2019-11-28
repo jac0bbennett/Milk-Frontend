f715b22e39dffefb4de3871e5527fd23fda6130b
Jacob Bennett <jacobwbennett@gmail.com>
2019-11-28 00:36:58 -0700

    Simlified saved to draft message

--- src/components/Views/Content/Fields/fieldMsg.js
+++ src/components/Views/Content/Fields/fieldMsg.js
@@ -4,14 +4,26 @@ const FieldMsg = props => {
   const [classes, setClasses] = useState(props.msg);
 
   useEffect(() => {
-    if (props.msg.includes("Saved")) setClasses("greentext");
+    if (props.msg.includes("Saved")) setClasses("bluetext");
     else if (props.msg === "saving...") setClasses("");
     else setClasses("redtext");
   }, [props.msg]);
 
   return (
     <span style={{ fontSize: "11pt" }} className={classes}>
-      {props.msg}
+      {props.msg.includes("Saved") ? (
+        <React.Fragment>
+          <i
+            className="material-icons"
+            style={{ fontSize: "14pt", cursor: "default" }}
+            title={props.msg}
+          >
+            check_circle
+          </i>
+        </React.Fragment>
+      ) : (
+        props.msg
+      )}
     </span>
   );
 };
